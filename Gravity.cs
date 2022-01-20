f0fc4a5083a9a852d93840b255332a1bda16b02a
MetricIsWashed <Ayman.Hachkal@gmail.com>
2022-01-20 08:07:08 +0400

    Master

--- Gravity.cs
+++ Gravity.cs
@@ -10,10 +10,10 @@ namespace gravity
         private Random random = new Random();
         double Gconst = 6.673889 * Math.Pow(10, -11);
         CircleShape circle;
-        float time = 0.1;
+        double time = 0.1;
         float mass;
-        float xvelocity = Random.Next(100);
-        float yvelocity = Random.Next(100);
+        float xvelocity;
+        float yvelocity;
         int Currentx;
         int Currenty;
         float Ay;
@@ -27,42 +27,50 @@ namespace gravity
             circle.Position = new Vector2f(xlocation, ylocation);
             Currentx = xlocation;
             Currenty = ylocation;
+            xvelocity = random.Next(100);
+            yvelocity = random.Next(100);
             mass = circle.Radius;
-            double fieldStrength = (Gconst*mass*M2);
+            //double fieldStrength = (Gconst*mass*M2);
         }
 
-        public void xmovement(int BodyCount,int SelfCount,List Body)
+        public void xmovement(object Body)
         {
-            float distancey = Math.Abs(Currenty - Body[i].Currenty); 
-            float distancex = Math.Abs(Currentx - Body[i].Currentx); 
+            float distancey = Math.Abs(Currenty - Body.getCurrenty); 
+            float distancex = Math.Abs(Currentx - Body.getCurrentx); 
             float distance =  Math.Sqrt(Math.Pow(distancex,2) + Math.Pow(distancey,2));
-            Ax += -mass*(Currentx-Body[i].Currentx)/Math.Pow(distance,3);
-            
+            Ax += -mass*(Currentx-Body.Currentx)/Math.Pow(distance,3);
+        }
+
+        public void ymovement(object Body)
+        {
+            float distancey = Math.Abs(Currenty - Body.Currenty); 
+            float distancex = Math.Abs(Currentx - Body.Currentx); 
+            float distance =  Math.Sqrt(Math.Pow(distancex,2) + Math.Pow(distancey,2));
+            Ay += -mass*(Currenty-Body.Currenty)/Math.Pow(distance,3);
+        }
+
+        public void LocationCalc()
+        {
             xvelocity += time*Ax;
             Currentx += time*xvelocity;
-            circle.Position = new Vector2f(Currentx, Currenty);
-        }
 
-        public void ymovement(int BodyCount,int SelfCount,List Body)
-        {
-            for (int i = 0; i < BodyCount; i++)
-            {
-                if(i!=SelfCount)
-                {
-                    float distancey = Math.Abs(Currenty - Body[i].Currenty); 
-                    float distancex = Math.Abs(Currentx - Body[i].Currentx); 
-                    float distance =  Math.Sqrt(distancex^2 + distancey^2);
-                    Ay += -mass*(Currenty-Body[i].Currenty)/distance^3;
-                }
-            }
-        
             yvelocity += time*Ay;
             Currenty += time*yvelocity;
             circle.Position = new Vector2f(Currentx, Currenty);
+
         }
         public Drawable returngrad()
         {
             return circle;
+        }
+
+        public int getCurrenty()
+        {
+            return Currenty;
+        }
+        public int getCurrentx()
+        {
+            return Currentx;
         }
 
     }
